997ed3dbe552e1e909a2a48e940f952b3f7285bd
Bronson Brown <Brsn90@icloud.com>
2020-03-05 19:57:08 -0700

    Add files via upload
    Inspiration from freeCodeCamp's implementation.

---
+++ breadth-first-search.js
@@ -0,0 +1,16 @@
+/*
+Graph Traversal Algorithm
+Distances between two nodes in a graph
+Visits edges
+*/
+
+
+
+var exBFSGraph = [
+    //0 1  2  3  4   
+    [0, 1, 1, 1, 0], //0
+    [0, 0, 1, 0, 0], //1
+    [1, 1, 0, 0, 0], //2
+    [0, 0, 0, 1, 0], //3
+    [0, 1, 0, 0, 0]  //4
+];
